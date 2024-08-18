d238c9e00c5bae89b4bd1005e68e1444b6a6c1e2
rezwanhossen <rezwanhossen625@gmail.com>
2024-08-18 19:39:44 +0600

    contact

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,12 @@ import logo from "../assets/logo.jpg";
 const Home = () => {
   return (
     <div>
-      <img className=" h-[100vh] w-full" src={logo} alt="" />
+      <div>
+        <img className=" h-[100vh] w-full" src={logo} alt="" />
+      </div>
+      <div className="mt-10">
+        <h2 className=" text-2xl font-bold text-center">Contact</h2>
+      </div>
     </div>
   );
 };
