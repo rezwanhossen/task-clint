4ab214b9f67d05082ff297df66aabfea30c257b6
rezwanhossen <rezwanhossen625@gmail.com>
2024-08-18 19:36:49 +0600

    add footer

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navber from "./Components/Navbar/Navber";
 import { Outlet } from "react-router-dom";
+import Footer from "./Components/Footer";
 
 const Root = () => {
   return (
     <div className="max-w-6xl mx-auto">
       <Navber></Navber>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
